46055c445334f27f23a5be116b69d84cf2377083
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-19 12:34:23 -0700

    New changes using the param keyword

--- Program.cs
+++ Program.cs
@@ -2,7 +2,21 @@
 {
     static void Main(string[] args)
     {
-
-        
+        int[] MyArray ={1,2,3,4,5,6,7,8};
+        ParamsArray(MyArray);
+        ParamsArray(5,6,7,8,9);
     }
+
+
+    static void ParamsArray(params int[] Array)
+    {
+        for (int i = 0; i < Array.Length; i++)
+        {
+
+            Console.WriteLine(Array[i]);
+
+        }
+
+    }
+
 }
